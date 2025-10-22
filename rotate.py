2d9e7254e232b19a9d0c2b0400059df55ac5c0dd
praths03 <59578361+praths03@users.noreply.github.com>
2025-10-23 02:48:12 +0530

    Create rotate.py

---
+++ rotate.py
@@ -0,0 +1,8 @@
+# Python program to check if a number is odd or even
+# Input from the user
+num = int(input("Enter a number: "))
+# Check if the number is even or odd
+if num % 2 == 0:
+   print(f"{num} is Even")
+else:
+   print(f"{num} is Odd")
